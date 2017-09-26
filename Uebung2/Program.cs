5b4025d4447a0afce13c1ae4cba0b8f25d00642a
Simon <simon_hischier@hotmail.com>
2017-09-26 19:15:36 +0200

    added Practice 2 Project

---
+++ Uebung2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test(1, 2);
+            test(1, 2, 3);
+            Console.Read();
+        }
+
+        // test method signature
+        public static void test(params int[]a)
+        {
+            Console.WriteLine("params");
+        }
+
+        public static void test(int a, int b)
+        {
+            Console.WriteLine("int int");
+        }
+    }
+}
