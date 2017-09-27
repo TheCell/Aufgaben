cb878842491a70eb421abd6dacb829a00b82d02a
Simon <simon_hischier@hotmail.com>
2017-09-27 13:04:42 +0200

    Aufgabe 2

---
+++ Uebung2/StringList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung2
+{
+    class StringList
+    {
+		protected string[] data;
+
+		public StringList()
+		{
+			data = new string[0];
+		}
+
+		public int Size
+		{
+			get
+			{
+				return data.Length;
+			}
+
+			set
+			{
+				data = new string[value];
+				for (int i = 0; i < data.Length; i++)
+				{
+					data[i] = "<empty>";
+				}
+			}
+		}
+
+		public string[] Data
+		{
+			get
+			{
+				return data;
+			}
+		}
+		
+        public string this [int index]
+        {
+            get
+            {
+                return data[index];
+            }
+
+            set
+            {
+                data[index] = value;
+            }
+        }
+    }
+}
