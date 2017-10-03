a4d38039b604b4fcb66644c2bb5f4e3fa3cf0aba
TheCell <simon_hischier@hotmail.com>
2017-10-03 20:43:24 +0200

    added practice unsolved files

---
+++ Threads/Counter/Counter.cs
@@ -0,0 +1,12 @@
+namespace Counter {
+
+    class Counter {
+
+        private int count = 0;
+
+        public int NextNumber() {
+            count++;
+            return count;
+        }
+    }
+}
