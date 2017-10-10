51cfe1d2b1d39f06c0e81a0d0ef32ec64aa7dab6
TheCell <simon_hischier@hotmail.com>
2017-10-10 18:57:05 +0200

    added SW4 Uebung, Delegates

---
+++ Uebung4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung4
+{
+	class Program
+	{
+		public delegate void myDelegate(string msg);
+		private myDelegate del;
+
+		static void Main(string[] args)
+		{
+			Program p = new Program();
+			p.test();
+		}
+
+		public void printMessage(string msg)
+		{
+			Console.WriteLine("Delegate called: " + msg);
+		}
+
+		void test()
+		{
+			del = printMessage;
+			del += printMessage;
+
+			del("myDelegate");
+
+			Delegate[] items = del.GetInvocationList();
+			int i = 0;
+			foreach(myDelegate item in items)
+			{
+				item("Aufruf " + ++i);
+				item.Invoke("Aufruf " + i); // Alternative
+			}
+		}
+	}
+}
